0b4df519a6d985def3e9506f3715e49eadf345d0
leoAlves007 <leonardoalvesfuzinelli@gmail.com>
2026-02-12 11:28:55 -0300

    Remove arquivos CSS desnecessários e refatora o componente App para incluir o Perfil

--- 03-devlinks/vite.config.js
+++ 03-devlinks/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,             // Muda porta padrão vite(5173)para o padrão react(3000)
+    open: true,            // Abre o navegador automaticamente
+    host: true,           // Permite acesso via IP na rede local
+  },
 })
