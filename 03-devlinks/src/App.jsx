baa28519d366a0338d0ee4445f9cc80145c118fe
leoAlves007 <leonardoalvesfuzinelli@gmail.com>
2026-02-12 16:55:58 -0300

    Adiciona componentes de Link, Rodape e SocialLink, e refatora o componente Perfil para aceitar props de imagem e nome de usuário.

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,20 +1,33 @@
 
 import './App.css'
+import Link from './assets/components/Link/link'
 import Perfil from './assets/components/perfil/Perfil'
+import Rodape from './assets/components/Rodape/Rodape'
+import SocialLink from './assets/components/SocialLink/SocialLink'
 
 function App() {
 
   return (
    <div id= 'App'>
- <Perfil></Perfil>
+ <Perfil fotoPerfil={"https://placehold.co/100"} >Leonardo Fuzinelli </Perfil>
 
   
-   <div className="switch">
-    botão switch 
-    </div>
-   <div className='links'></div>
+   <div className="switch">botão switch </div>
+
+   <ul>
+    <Link url={""}>Inscreva-se</Link>
+    <Link url={""}>Minha Playlist</Link>
+    <Link url={""}>Me pague um café!</Link>
+    <Link url={""}>Conheça o meu Curso DEV</Link>
+   </ul>
+
+<div className='links'></div>
+<SocialLink url={"https://hithub.com"} icon={"logo-github"} />
+<SocialLink  url={"https://instagram.com"} icon={"logo-instagram"}/>
+<SocialLink  url={"https://youtube.com"} icon={"logo-youtube"}/>
+<SocialLink  url={"https://br.linkedin.com"} icon={"logo-linkedin"}/>
+<Rodape>Leonardo Fuzinelli</Rodape>
    <div className='socialLinks'></div>
-   <div className='rodape'></div>
    </div> 
 
   )
