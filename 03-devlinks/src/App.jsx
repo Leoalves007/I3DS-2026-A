a07b3178175e644d1ee75a7081bc482107cc9821
leoAlves007 <leonardoalvesfuzinelli@gmail.com>
2026-02-13 14:49:08 -0300

    Refatora estilos e estrutura dos componentes: App, Perfil, Rodape e Link; adiciona novo componente Switch

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,37 +1,35 @@
-
-import './App.css'
-import Link from './components/Link/link'
-import Perfil from './components/perfil/Perfil'
-import Rodape from './components/Rodape/Rodape'
-import SocialLink from './components/SocialLink/SocialLink'
+import "./App.css";
+import Link from "./components/Link/link";
+import Perfil from "./components/perfil/Perfil";
+import Rodape from "./components/Rodape/Rodape";
+import SocialLink from "./components/SocialLink/SocialLink";
 
 function App() {
-
   return (
-   <div id= 'App'>
- <Perfil fotoPerfil={"https://placehold.co/100"} >Leonardo Fuzinelli </Perfil>
+    <div id="App">
+      <Perfil fotoPerfil={"https://placehold.co/100"}>
+        Leonardo Fuzinelli{" "}
+      </Perfil>
 
-  
-   <div className="switch">botão switch </div>
-   <div id="Link">
+      <div className="switch">botão switch </div>
+      <div id="Link">
+        <ul>
+          <Link url={""}>Inscreva-se</Link>
+          <Link url={""}>Minha Playlist</Link>
+          <Link url={""}>Me pague um café!</Link>
+          <Link url={""}>Conheça o meu Curso DEV</Link>
+        </ul>
+      </div>
 
-   <ul>
-    <Link url={""}>Inscreva-se</Link>
-    <Link url={""}>Minha Playlist</Link>
-    <Link url={""}>Me pague um café!</Link>
-    <Link url={""}>Conheça o meu Curso DEV</Link>
-   </ul>
-</div>
-<div className='links'></div>
-<SocialLink url={"https://hithub.com"} icon={"logo-github"} />
-<SocialLink  url={"https://instagram.com"} icon={"logo-instagram"}/>
-<SocialLink  url={"https://youtube.com"} icon={"logo-youtube"}/>
-<SocialLink  url={"https://br.linkedin.com"} icon={"logo-linkedin"}/>
-<Rodape>Leonardo Fuzinelli</Rodape>
-   <div className='socialLinks'></div>
-   </div> 
-
-  )
+      <div id="SocialLinks">
+        <SocialLink url={"https://hithub.com"} icon={"logo-github"} />
+        <SocialLink url={"https://instagram.com"} icon={"logo-instagram"} />
+        <SocialLink url={"https://youtube.com"} icon={"logo-youtube"} />
+        <SocialLink url={"https://br.linkedin.com"} icon={"logo-linkedin"} />
+      </div>
+        <Rodape>Leonardo Fuzinelli</Rodape>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ 03-devlinks/src/components/Switch/Switch.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./Switch.module.css";
+
+const Switch = (troca, isLight) => {
+  return (
+    <div className={isLight ? StyleSheet.light: ""}>
+        <div id={styles.Switch}>
+     <button
+
+        </div>
+        
+      
+    </div>
+  )
+}
+
+export default Switch
