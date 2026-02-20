b52a5f6bb4a4a22299b5c9d178de765d4a3fa493
leoAlves007 <leonardoalvesfuzinelli@gmail.com>
2026-02-20 14:24:00 -0300

    Implementa busca de filmes com integração à API OMDB e adiciona novos componentes de exibição

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,9 +1,68 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+
+import Rodape from "./components/Rodape/Rodape";
+import MovieCard from "./components/MovieCard/MovieCard";
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  //Utilizando uma CHAVE de API do arquivo .env
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+  //Criando a conexão com a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
+
+    //Alimentando a variavel movies
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Barbie");
+  }, []);
+
   return (
-    <div>App</div>
-  )
+    <div id="App">
+      <img
+        id="Logo"
+        src={logo}
+        alt="Logotipo do serviço de streaming Devflix, com letras vermelhas e fundo preto, promovendo conteúdo de séries, filmes e entretenimento online."
+      />
+
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes" />
+        <img src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+
+{movies?.length > 0?
+(
+ <div className="container">
+        {movies.map((movie, index) => (
+          <MovieCard key={index} {...movie} />
+        ))}
+      </div>
+)
+:
+(
+  <h2>🥺 Filme não encontrado 🥺</h2>
+)
 }
 
-export default App
+      <div className="container">
+        {movies.map((movie, index) => (
+          <MovieCard key={index} {...movie} />
+        ))}
+      </div>
+
+      <Rodape link={"https://github.com/Leoalves007"}>eoAlves</Rodape>
+    </div>
+  );
+};
+
+export default App;
